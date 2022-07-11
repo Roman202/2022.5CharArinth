0c3312d17a6911a7efba577287e785a73011256a
Roman Hera <romahera6@gmail.com>
2022-07-11 20:24:32 +0300

    Add files via upload

---
+++ CharArithDemo.java
@@ -0,0 +1,18 @@
+
+public class CharArithDemo {
+	
+	public static void main(String[] args) {
+		char ch;
+		
+		ch = 'L';
+		System.out.println("ch рівне " + ch);
+		
+		ch++;
+		System.out.println("значення ch змінилось на " + ch);
+		
+		ch = 'D';
+		ch--;
+		System.out.println("значення ch знову змінилось і рівне " + ch);
+	}
+
+}
